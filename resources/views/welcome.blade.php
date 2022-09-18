ba09794342fe83b4240b48b8fc48cafaf0590ba4
greis <tonyxhepa.greis@gmail.com>
2022-09-18 11:54:45 +0200

    about section added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,9 @@
     {{-- Hero primary --}}
     <x-frontend.hero></x-frontend.hero>
     {{-- Promote tail-100 --}}
+    <x-frontend.promote></x-frontend.promote>
     {{-- About Secondary --}}
+    <x-frontend.about></x-frontend.about>
     {{-- Skills tail-100 --}}
     {{-- Portfolio primary --}}
     {{-- Services secondary --}}
